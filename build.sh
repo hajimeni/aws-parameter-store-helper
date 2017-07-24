d38c6ee26cf73818d0b6765c3bf65dd3fafc0fae
hajimeni <nishiyama@bizreach.co.jp>
2017-07-24 17:21:16 +0900

    add travis ci config

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd $(dirname $(basename $0))
+echo "Build directory is $(pwd)"
+OS=("darwin" "linux" "windows")
+ARCH="amd64"
+NAME="aws-ps"
+
+for o in ${OS[@]}; do
+    echo "Build start for ${o}-${ARCH}"
+    GOOS=$o GOARCH=$ARCH go build -a -o bin/$NAME
+    tar -czvf bin/$NAME-$o-$ARCH.tar.gz -C bin $NAME
+    echo "Build succeed !! for ${o}-${ARCH}"
+    rm bin/$NAME
+done
+
+echo $(ls -al bin)
